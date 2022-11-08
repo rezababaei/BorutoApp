b33198136347feaad3d32d274853df76bc6187db
Reza Babaei <rzbabaei1@gmail.com>
2022-11-08 11:17:26 +0100

    adding hero remote key and it's dao

--- app/src/main/java/com/rbabaei/borutoapp/data/BorutoDatabase.kt
+++ app/src/main/java/com/rbabaei/borutoapp/data/BorutoDatabase.kt
@@ -2,9 +2,12 @@ package com.rbabaei.borutoapp.data
 
 import androidx.room.Database
 import com.rbabaei.borutoapp.data.dao.HeroDao
+import com.rbabaei.borutoapp.data.dao.HeroRemoteKeyDao
 import com.rbabaei.borutoapp.domain.model.Hero
+import com.rbabaei.borutoapp.domain.model.HeroRemoteKey
 
-@Database(entities = [Hero::class], version = 1)
+@Database(entities = [Hero::class,HeroRemoteKey::class], version = 1)
 abstract class BorutoDatabase {
     abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao():HeroRemoteKeyDao
 }
---
+++ app/src/main/java/com/rbabaei/borutoapp/domain/model/HeroRemoteKey.kt
@@ -0,0 +1,13 @@
+package com.rbabaei.borutoapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.rbabaei.borutoapp.util.Constants.HERO_REMOTE_KEY_DATABASE_TABLE
+
+@Entity(tableName = HERO_REMOTE_KEY_DATABASE_TABLE)
+data class HeroRemoteKey(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val prevKey:Int?,
+    val nextKey:Int?
+)
