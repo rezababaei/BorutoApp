c853f33093982a26e3b8f1bca5b661d405fdda09
Reza Babaei <rzbabaei1@gmail.com>
2022-11-08 11:47:43 +0100

    Dagger_Hilt added to the project and has been setup to provide instance for Room database

--- app/src/main/java/com/rbabaei/borutoapp/MainActivity.kt
+++ app/src/main/java/com/rbabaei/borutoapp/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.rbabaei.borutoapp.navigation.SetupNavGraph
 import com.rbabaei.borutoapp.ui.theme.BorutoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private lateinit var navController:NavHostController
 
--- app/src/main/java/com/rbabaei/borutoapp/data/BorutoDatabase.kt
+++ app/src/main/java/com/rbabaei/borutoapp/data/BorutoDatabase.kt
@@ -1,6 +1,7 @@
 package com.rbabaei.borutoapp.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.rbabaei.borutoapp.data.dao.HeroDao
 import com.rbabaei.borutoapp.data.dao.HeroRemoteKeyDao
@@ -9,7 +10,7 @@ import com.rbabaei.borutoapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class,HeroRemoteKey::class], version = 1)
 @TypeConverters(DatabaseConvertor::class)
-abstract class BorutoDatabase {
+abstract class BorutoDatabase: RoomDatabase()  {
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeyDao():HeroRemoteKeyDao
 }
